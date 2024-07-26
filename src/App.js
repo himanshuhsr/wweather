e739676cfb55740ed7cce09be98e49c0b9bb196e
Himanshu Raj <rajh91774@gmail.com>
2024-07-26 10:44:35 +0530

    Weather App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { Card, Col, Container, FormControl, InputGroup, Row, Button,Alert } from 'react-bootstrap';
+import axios from 'axios';
 
-function App() {
+const App = () => {
+  const [query, setQuery] = useState('');
+  const [error, setError] = useState('');
+  const [loader, setLoader] = useState(false);
+  const [data, setData] = useState(null);
+
+  const fetchData = async() =>{
+    setData(null);
+    setError('');
+    setLoader(true);
+    if(query.length < 3){
+      setError('Please enter correct city or zip code.');
+      setLoader(false);
+    }else{
+      try{
+        const response = await axios.get(`http://api.weatherapi.com/v1/current.json?key=${process.env.REACT_APP_API_KEY}&q=${query}&aqi=yes`);
+      setData(response.data);
+    console.log(response.data);
+      setLoader(false);
+      }catch(err){
+        setError(err.response.data.error.message);
+      }
+    }
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Container>
+      <Row className='weather-card'>
+        <Col lg={9} md={10}>
+          <Card className='p-4 shadow weather-data'>
+            <div className='search-bar'>
+              <InputGroup>
+                <InputGroup.Text id='basic-addon1'>
+                  <i className='bi bi-search'></i>
+                </InputGroup.Text>
+                <FormControl onChange={(e)=>setQuery(e.target.value)} placeholder='Enter city name/zip code...' />
+                <Button disabled={loader} onClick={fetchData}>Search</Button>
+              </InputGroup>
+            </div>
+            <div className='p-2'>
+              {error ? <Alert variant={'danger'} key="danger">{error}</Alert> : ''}
+            </div>
+            <Container>
+              {data ? (
+                <div className='py-4'>
+                <span className='d-block text-center'><i className='bi bi-geo-alt'></i> {data.location.name}, {data.location.region}, {data.location.country}</span>
+
+              <span className='text-blue d-block text-center mt-2'><i className='bi bi-clock'></i> {data.location.tz_id}</span>
+                <div className='my-4'>
+                <h5 className='text-blue'>Air Quality:</h5>
+                <hr />
+                <div className='d-lg-flex justify-content-between mt-3'>
+                  <span><span className='text-blue'>CO : </span><span>{data.current.air_quality.co}</span></span> 
+                  <span> | </span>
+                  <span><span className='text-blue'>Defra Index : </span><span>{data.current.air_quality['gb-defra-index']}</span></span>
+                  <span> | </span>
+                  <span><span className='text-blue'>NO<sub>2</sub> : </span><span>{data.current.air_quality.no2}</span></span>
+                  <span> | </span>
+                  <span><span className='text-blue'>O<sub>3</sub> : </span><span>{data.current.air_quality.o3}</span></span>
+                  <span> | </span>
+                  <span><span className='text-blue'>PM<sub>2.5</sub> : </span><span>{data.current.air_quality.pm2_5}</span></span>
+                  <span> | </span>
+                  <span><span className='text-blue'>PM<sub>10</sub> : </span><span>{data.current.air_quality.pm10}</span></span>
+                  <span> | </span>
+                  <span><span className='text-blue'>SO<sub>2</sub> : </span><span>{data.current.air_quality.so2}</span></span>
+                </div>
+                </div>
+                </div>
+              ) : <span></span>}
+
+              {loader ? <span>Fetching data...</span>: ''}
+            </Container>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  )
 }
 
-export default App;
+export default App
